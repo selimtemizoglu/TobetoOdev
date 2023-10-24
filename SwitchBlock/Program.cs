6be5dfabae4fe6861bc42816d87680ec235f3f8b
yavuzselimtemizoglu <yavuzselimtemizoglu@hotmail.com>
2023-10-24 22:44:30 +0300

    SwitchBlock

---
+++ SwitchBlock/Program.cs
@@ -0,0 +1,18 @@
+﻿var number = 11;
+
+switch (number)
+{
+    case 10:
+        Console.WriteLine("Number is 10");
+        break;
+    case 20:
+        Console.WriteLine("Number is 20");
+        break;
+    default:
+        Console.WriteLine("Number not is 10 or 20");
+        break;
+
+       
+
+}
+Console.ReadLine();
