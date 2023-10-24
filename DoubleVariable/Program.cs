c24f0b557fe74e86e580fad0699985aef29730a5
yavuzselimtemizoglu <yavuzselimtemizoglu@hotmail.com>
2023-10-24 21:47:05 +0300

    DoubleVariable

---
+++ DoubleVariable/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+int number1 = 10;
+long number2 = 2147483648;
+short number3 = 32767;
+byte number4 = 255;
+double number5 = 10.4;
+char character = 'A';
+Console.WriteLine("Number 1 is {0}", number1);
+Console.WriteLine("Number 2 is {0}", number2);
+Console.WriteLine("Number 3 is {0}", number3);
+Console.WriteLine("Number 4 is {0}", number4);
+Console.WriteLine("Character is {0}", character);
+Console.WriteLine("Number 5 is {0}", number5);
+
+Console.ReadLine();
+
