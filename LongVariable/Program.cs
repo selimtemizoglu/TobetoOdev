0989e9cde7e03bab5a150b2e1378c2539cd728ef
yavuzselimtemizoglu <yavuzselimtemizoglu@hotmail.com>
2023-10-24 18:36:43 +0300

    Long Variable

---
+++ LongVariable/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int number1 = 10;
+long number2 = 2147483648;
+Console.WriteLine("Number 1 is {0}", number1);
+Console.WriteLine("Number 2 is {0}", number2);
+
+Console.ReadLine();
