a55235202c6ff1257633c6c1c8af10ce4659f86f
yavuzselimtemizoglu <yavuzselimtemizoglu@hotmail.com>
2023-10-24 22:37:49 +0300

    IfElseBlock

---
+++ IfElseBlock/Program.cs
@@ -0,0 +1,15 @@
+﻿var number = 10;
+
+if (number == 10)
+{
+    Console.WriteLine("Number is 10");
+}
+else if( number == 20)
+{
+    Console.WriteLine("Number is 20");
+}
+else
+{
+    Console.WriteLine("Number is not 10 or 20");
+}
+Console.ReadLine();
