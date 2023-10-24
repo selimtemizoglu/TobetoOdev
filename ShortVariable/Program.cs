daebaa6e227f8e1e6e5ec5abcef03a6c2d0c4013
yavuzselimtemizoglu <yavuzselimtemizoglu@hotmail.com>
2023-10-24 21:27:56 +0300

    ShortVariable

---
+++ ShortVariable/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+short number3 = 32767;
+int number1 = 10;
+long number2 = 2147483648;
+Console.WriteLine("Number 1 is {0}", number1);
+Console.WriteLine("Number 2 is {0}", number2);
+Console.WriteLine("Number 3 is {0}", number3);
+
+Console.ReadLine();
+
+
