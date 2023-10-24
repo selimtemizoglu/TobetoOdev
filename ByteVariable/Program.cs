0a93d9a5b19b8e8d80be63c348957d8f04430a72
yavuzselimtemizoglu <yavuzselimtemizoglu@hotmail.com>
2023-10-24 21:31:15 +0300

    Byte Variable

---
+++ ByteVariable/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int number1 = 10;
+long number2 = 2147483648;
+short number3 = 32767;
+byte number4 = 255;
+Console.WriteLine("Number 1 is {0}", number1);
+Console.WriteLine("Number 2 is {0}", number2);
+Console.WriteLine("Number 3 is {0}", number3);
+Console.WriteLine("Number 4 is {0}", number4);
+
+Console.ReadLine();
