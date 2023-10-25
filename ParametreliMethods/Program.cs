2bc8bfa292f58700e9e1649114fe05f4a801f1fb
yavuzselimtemizoglu <yavuzselimtemizoglu@hotmail.com>
2023-10-25 10:16:16 +0300

    ParametreliMethods

---
+++ ParametreliMethods/Program.cs
@@ -0,0 +1,16 @@
+﻿static void Add()
+{
+    Console.WriteLine("Added!");
+}
+Add();
+Add();
+var result = Add2(2,4);
+Console.WriteLine(result);
+static int Add2(int number1, int number2)
+{
+    var result = number1 + number2;
+    return result;
+}
+
+Console.ReadLine();
+
