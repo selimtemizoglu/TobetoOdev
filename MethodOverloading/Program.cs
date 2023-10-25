f6ffb1d418a90ac61724acf4ad940d5e8ada2f1b
yavuzselimtemizoglu <yavuzselimtemizoglu@hotmail.com>
2023-10-25 11:13:20 +0300

    MethodOverloading

---
+++ MethodOverloading/Program.cs
@@ -0,0 +1,40 @@
+﻿static void Add()
+{
+    Console.WriteLine("Added!");
+}
+Add();
+Add();
+var result = Add2(2);
+Console.WriteLine(result);
+
+//int number1;
+//int number2 = 100;
+//var result2 = Add3(out number1, number2);
+//Console.WriteLine(result2);
+//Console.WriteLine(number1);
+Console.WriteLine(Multiply(2,4));
+
+static int Add2(int x, int number1 = 12, int number2 = 15)
+{
+    var result = number1 + number2;
+    
+
+
+    return result;
+}
+
+static int Add3(out int number1, int number2)
+{
+    return number1 = 30 + number2;
+}
+
+static int Multiply (int number1,int number2 )
+{
+    return number1 * number2;
+}
+static int Multiply2(int number1, int number2, int number3)
+{
+    return number1 * number2 * number3;
+}
+Console.ReadLine();
+
