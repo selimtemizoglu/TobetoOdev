5bbf081a30d29b64bf1710483cdd594632e0747e
yavuzselimtemizoglu <yavuzselimtemizoglu@hotmail.com>
2023-10-25 12:36:29 +0300

    Arrays

---
+++ Arrays/Program.cs
@@ -0,0 +1,11 @@
+﻿string[] students = new string[3] { "Engin", "Derin", "Salih" };
+
+string[] students2 = { "Engin", "Derin", "Salih" };
+students2[3] = "Ahmet";
+
+foreach (var student in students2)
+{
+    Console.WriteLine(student);
+}
+Console.WriteLine();
+Console.ReadLine();
