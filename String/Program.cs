a02a1f859830e774ace041fbd4d869260766f4b9
yavuzselimtemizoglu <yavuzselimtemizoglu@hotmail.com>
2023-10-25 15:14:47 +0300

    String

---
+++ String/Program.cs
@@ -0,0 +1,12 @@
+﻿string city = "Ankara";
+//Console.WriteLine(city[0])
+
+foreach (var item in city)
+{
+    Console.WriteLine(item);
+}
+
+string city2 = "İstanbul";
+
+Console.WriteLine(String.Format("{0}{1}",city,city2));
+Console.ReadLine();
