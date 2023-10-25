e844e6218e91a80e348d0deb5061a8e65d33f119
yavuzselimtemizoglu <yavuzselimtemizoglu@hotmail.com>
2023-10-25 10:44:11 +0300

    DefaultParametreliMethod

---
+++ DefaultParametreliMethod/Program.cs
@@ -0,0 +1,15 @@
+﻿static void Add()
+{
+    Console.WriteLine("Added!");
+}
+Add();
+Add();
+var result = Add2(2);
+Console.WriteLine(result);
+static int Add2(int x,int number1 = 12, int number2 = 15)
+{
+    var result = number1 + number2;
+    return result;
+}
+
+Console.ReadLine();
