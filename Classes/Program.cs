ad3b8e62cd2fad141dfcb6f65e35f899bc7c95b5
yavuzselimtemizoglu <yavuzselimtemizoglu@hotmail.com>
2023-10-27 08:45:52 +0300

    Classes

---
+++ Classes/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Classes;
+
+
+CustomerManager customerManager = new CustomerManager();
+
+customerManager.Add();
+customerManager.Update();
+
+ProductManager productManager = new ProductManager();
+productManager.Add();
+productManager.Update();
+
+Customer customer = new Customer();
+
+customer.City = "İstanbul";
+customer.Id = 1;
+customer.FirstName = "Yavuz Selim";
+customer.LastName = "Temizoğlu";
+
+Customer customer1 = new Customer
+{
+    Id = 2, City = "Ankara", FirstName = "Engin", LastName = "Demiroğ"
+};
+
+Console.WriteLine(customer1.FirstName);
+
+Console.ReadLine();
+
+
+
+
+
