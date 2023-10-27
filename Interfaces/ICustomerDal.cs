6b54626b796393bfd3d5a5862d8f032c33227f5a
yavuzselimtemizoglu <yavuzselimtemizoglu@hotmail.com>
2023-10-27 11:20:39 +0300

    Interfaces

---
+++ Interfaces/ICustomerDal.cs
@@ -0,0 +1,53 @@
+﻿using System;
+namespace Interfaces
+{
+	public interface ICustomerDal
+	{
+		void Add();
+		void Update();
+		void Delete();
+	}
+
+    class SqlServerCustomerDal : ICustomerDal
+    {
+        void ICustomerDal.Add()
+        {
+            Console.WriteLine("Sql added");
+        }
+
+        void ICustomerDal.Delete()
+        {
+            Console.WriteLine("Sql Deleted");
+        }
+
+        void ICustomerDal.Update()
+        {
+            Console.WriteLine("Sqp updated");
+        }
+    }
+    class OracleCustomerDal : ICustomerDal
+    {
+        void ICustomerDal.Add()
+        {
+            Console.WriteLine("Oracle added");
+        }
+
+        void ICustomerDal.Delete()
+        {
+            Console.WriteLine("Oracle Deleted");
+        }
+
+        void ICustomerDal.Update()
+        {
+            Console.WriteLine("Oracle updated");
+        }
+    }
+    class CustomerManager
+    {
+        public void Add(ICustomerDal customerDal)
+        {
+            customerDal.Add();
+        }
+    }
+}
+
---
+++ Interfaces/Program.cs
@@ -0,0 +1,87 @@
+﻿
+using Interfaces;
+//InterfacesIntro();
+
+///Demo();
+///
+
+ICustomerDal[] customerDals = new ICustomerDal[2]
+{
+    new SqlServerCustomerDal(),
+    new OracleCustomerDal()
+};
+
+foreach (var customerDal in customerDals)
+{
+    customerDal.Add();
+}
+Console.ReadLine();
+
+static void InterfacesIntro()
+{
+    PersonManager manager = new PersonManager();
+    Customer customer = new Customer
+    {
+        Id = 1,
+        FirstName = "Yavuz Selim",
+        LastName = "Temizoğlu",
+        Adress = "İstanbul"
+    };
+
+    Student student = new Student
+    {
+        Id = 1,
+        FirstName = "Nazlı Gül",
+        LastName = "Temizoğlu",
+        Departmant = "Student"
+    };
+
+    manager.Add(customer);
+    manager.Add(student);
+}
+
+static void Demo()
+{
+    CustomerManager customerManager = new CustomerManager();
+    customerManager.Add(new OracleCustomerDal());
+}
+
+interface IPerson
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+}
+
+internal class Customer : IPerson
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Adress { get; set; }
+
+}
+
+class Student : IPerson
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Departmant { get; set; }
+}
+
+class Worker : IPerson
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Departmant { get; set; }
+}
+
+class PersonManager
+{
+    public void Add(IPerson person)
+    {
+        Console.WriteLine(person.FirstName);
+    }
+}
