9187a3983cc260c7e286a90ebc2b88896a4a29ba
yavuzselimtemizoglu <yavuzselimtemizoglu@hotmail.com>
2023-10-27 12:06:28 +0300

    Inheritance

---
+++ Inheritance/Program.cs
@@ -0,0 +1,31 @@
+﻿Person[] persons = new Person[3]
+{
+    new Customer{FirstName = "Yavuz Selim"}, new Student{FirstName = "Betül"}, new Person{FirstName = "Nazlı"}
+};
+
+foreach (var person in persons)
+{
+    Console.WriteLine(person.FirstName);
+}
+
+Console.ReadLine();
+
+
+
+class Person
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+}
+
+
+class Customer : Person
+{
+    public string City { get; set; }
+}
+
+class Student : Person
+{
+    public string Department { get; set; }
+}
