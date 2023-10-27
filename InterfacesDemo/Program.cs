8f4ccb5eac0d66fe00dce0c9ba2783d402de2930
yavuzselimtemizoglu <yavuzselimtemizoglu@hotmail.com>
2023-10-27 11:51:53 +0300

    InterfacesDemo

---
+++ InterfacesDemo/Program.cs
@@ -0,0 +1,78 @@
+﻿
+IWorker[] workers = new IWorker[3]
+{
+    new Manager(),
+    new Worker(),
+    new Robot()
+};
+
+foreach (var worker in workers)
+{
+    worker.Work();
+}
+
+IEat[] eats = new IEat[2]
+{
+    new Worker(),
+    new Manager()
+};
+
+interface IWorker
+{
+    void Work();
+    
+}
+
+interface IEat
+{
+    void Eat();
+}
+
+interface ISalary
+{
+    void GetSalary();
+}
+
+class Manager : IWorker, IEat, ISalary
+{
+    public void Eat()
+    {
+        throw new NotImplementedException();
+    }
+
+    void ISalary.GetSalary()
+    {
+        throw new NotImplementedException();
+    }
+
+    void IWorker.Work()
+    {
+        throw new NotImplementedException();
+    }
+}
+
+class Worker : IWorker, IEat, ISalary
+{
+    public void Eat()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void GetSalary()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Work()
+    {
+        throw new NotImplementedException();
+    }
+}
+
+class Robot : IWorker
+{
+    public void Work()
+    {
+        throw new NotImplementedException();
+    }
+}
